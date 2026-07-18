09d91f971d4047ab92820a21eae942ef40895299
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2026-07-18 22:42:21 +0900

    update build config and remove cjs support

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig([
   {
-    entry: ["src/build.ts", "src/sidebar.ts"],
+    entry: ["src/config/index.ts", "src/build.ts", "src/sidebar.ts"],
     format: ["esm"],
     dts: true,
     clean: false,
+    deps: {
+      onlyBundle: false,
+    },
   },
 ]);
