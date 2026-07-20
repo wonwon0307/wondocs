acbffbba4df17b2215b6668b6f9d04541c03c63b
정승원 <vietman2@jeongseung-won-ui-Macmini.local>
2026-07-20 13:52:40 +0900

    add buildDocs testcase

---
+++ packages/core/tests/unittests/build.test.ts
@@ -0,0 +1,54 @@
+import { buildDocs } from "@/build";
+import { detectCollections } from "@/collection/build";
+import { scanMeta } from "@/meta/scan";
+import { scanFileTree } from "@/filetree/scan";
+import { buildPages } from "@/filetree/build";
+import { atomicWrite } from "@/utils/files";
+
+describe("buildDocs", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const config = {
+    root: "/test-root",
+    contentsDir: "test-contents",
+    baseUrl: "/",
+  };
+
+  // everything is mocked, so only test if functions are called
+  it("should call functions correctly", async () => {
+    await buildDocs(config);
+
+    // 1. detect collections
+    expect(detectCollections).toHaveBeenCalledWith("test-contents");
+
+    // 2. scan
+    expect(scanMeta).toHaveBeenCalledWith(
+      "/path/to/group1/meta.json",
+      "group1",
+    );
+    expect(scanMeta).toHaveBeenCalledWith(
+      "/path/to/group2/meta.json",
+      "group2",
+    );
+    expect(scanFileTree).toHaveBeenCalledWith("/path/to/group1", "test-prefix");
+    expect(scanFileTree).toHaveBeenCalledWith("/path/to/group2", "test-prefix");
+
+    // 3. build pages
+    expect(buildPages).toHaveBeenCalledWith(
+      { "test-leaf": "/path/to/test-leaf.md" },
+      "/test-root/.wondocs",
+    );
+
+    // 4. write files
+    expect(atomicWrite).toHaveBeenCalledWith(
+      "/test-root/.wondocs/sidebar.js",
+      expect.stringContaining('"group1"'),
+    );
+    expect(atomicWrite).toHaveBeenCalledWith(
+      "/test-root/.wondocs/pages.js",
+      expect.stringContaining('"test-leaf"'),
+    );
+  });
+});
